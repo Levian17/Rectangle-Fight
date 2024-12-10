c9657033595bed60cfbd4bd32123bbfa0eb034dc
Carballo Otero <macarballootero@deloitte.es>
2024-12-10 15:20:29 +0100

    Cambio en los parametros iniciales.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
-from pygame.locals import *
+from pygame.locals import KEYDOWN, K_ESCAPE
 from objects.MovingRect import movingRects_generation
 
 # Parametros iniciales
+numero_cuadrados = 100
 width, height = 1080, 720
 screen = pygame.display.set_mode((width, height))
 running = True
 
 # Definicion de los rectangulos
-rects = movingRects_generation(100, width, height)
+rects = movingRects_generation(numero_cuadrados, width, height)
 
 pygame.init() # Se inicia el ciclo
 while running: # Ciclo frames
@@ -20,7 +21,7 @@ while running: # Ciclo frames
             if event.key == K_ESCAPE:
                 running = False
 
-    for rect in rects: # Desplazamos los rects
+    for rect in rects: # Controlamos el movimiento y la colison de los rects
         rect.move()
         rect.check_colision(rects)
 
